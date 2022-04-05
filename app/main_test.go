0b1d7743e486704423e9a80562d33353d2452723
Nil Borodulia <nborodulia@gmail.com>
2022-04-05 16:55:32 +0300

    change test

--- app/main_test.go
+++ app/main_test.go
@@ -13,16 +13,6 @@ import (
 
 func Test_Main(t *testing.T) {
 
-//     testServer := make(chan struct{})
-//
-//     go func() {
-//         <-testServer
-//         http.HandleFunc("/hello", func (w http.ResponseWriter, req *http.Request) {
-//             fmt.Fprintf(w, "hello\n")
-//         })
-//         http.ListenAndServe(":8090", nil)
-//     }()
-
     os.Args = []string{"main", "--target=http://google.com/"}
 //
     done := make(chan struct{})
@@ -43,18 +33,16 @@ func Test_Main(t *testing.T) {
         //<-finished
     }()
 
-    port := 8081;
-    resp, err := http.Get(fmt.Sprintf("http://127.0.0.1:%d/api/v1/ping", port))
+    port := 8081
+    host := "127.0.0.1"
+
+    resp, err := http.Get(fmt.Sprintf("http://%s:%d/api/v1/ping", host, port))
     response, _ := ioutil.ReadAll(resp.Body)
     require.NoError(t, err)
     assert.Equal(t, "pong", string(response))
 
-    respns, _ := http.Get(fmt.Sprintf("http://127.0.0.1:%d/", port))
-    resBody, _ := ioutil.ReadAll(respns.Body)
-    fmt.Printf(string(resBody))
+//     respns, _ := http.Get(fmt.Sprintf("http://%s:%d/", host, port))
+//     resBody, _ := ioutil.ReadAll(respns.Body)
+//     fmt.Printf(string(resBody))
 
-}
-
-func hello(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "hello\n")
 }
