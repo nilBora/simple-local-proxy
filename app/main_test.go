4335251804156711cce16d0ce2aa4d0cd6ad0460
Nil Borodulia <nborodulia@gmail.com>
2022-04-15 08:06:29 +0100

    cachnge test. Add target from digitalocean

--- app/main_test.go
+++ app/main_test.go
@@ -1,15 +1,15 @@
 package main
 import (
     "testing"
-    //"github.com/stretchr/testify/assert"
-    //"github.com/stretchr/testify/require"
-   // "os"
+    "github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/require"
+    "os"
     //"net"
     "net/http"
     "net/http/httptest"
- //   "fmt"
-//    "syscall"
-    //"io/ioutil"
+    "fmt"
+    "syscall"
+    "io/ioutil"
 )
 
 func TestPing(t *testing.T) {
@@ -28,38 +28,33 @@ func TestPing(t *testing.T) {
 	})
 }
 
-// func Test_Main(t *testing.T) {
+func Test_Main(t *testing.T) {
+
+    os.Args = []string{"main", "--target=http://147.182.244.37:8080/"}
 //
-//     os.Args = []string{"main", "--target=http://google.com/"}
-// //
-//     done := make(chan struct{})
-//     go func() {
-//         <-done
-//         e := syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
-//         require.NoError(t, e)
-//     }()
-//
-//     finished := make(chan struct{})
-//     go func() {
-//         main()
-//         close(finished)
-//     }()
-//
+    done := make(chan struct{})
+    go func() {
+        <-done
+        e := syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
+        require.NoError(t, e)
+    }()
+
+    finished := make(chan struct{})
+    go func() {
+        main()
+        close(finished)
+    }()
+
 //     defer func() {
 //         close(done)
-//        // <-finished
+//         <-finished
 //     }()
-//
-//     port := 8081
-//     host := "127.0.0.1"
-//
-//     resp, err := http.Get(fmt.Sprintf("http://%s:%d/api/v1/ping", host, port))
-//     response, _ := ioutil.ReadAll(resp.Body)
-//     require.NoError(t, err)
-//     assert.Equal(t, "pong", string(response))
-//
-// //     respns, _ := http.Get(fmt.Sprintf("http://%s:%d/", host, port))
-// //     resBody, _ := ioutil.ReadAll(respns.Body)
-// //     fmt.Printf(string(resBody))
-//
-// }
+
+    port := 8081
+    host := "127.0.0.1"
+
+    resp, err := http.Get(fmt.Sprintf("http://%s:%d/ping", host, port))
+    response, _ := ioutil.ReadAll(resp.Body)
+    require.NoError(t, err)
+    assert.Equal(t, "pong", string(response))
+}
