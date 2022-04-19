869b48771ceecc380182ddf384272122da73bf1c
Nil Borodulia <nborodulia@gmail.com>
2022-04-19 07:52:00 +0100

    remove comments

--- app/main.go
+++ app/main.go
@@ -85,20 +85,6 @@ func Ping(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "pong")
 }
 
-// func Ping(next http.Handler) http.Handler {
-// 	fn := func(w http.ResponseWriter, r *http.Request) {
-//
-// 		if r.Method == "GET" && strings.HasSuffix(strings.ToLower(r.URL.Path), "/ping") {
-// 			w.Header().Set("Content-Type", "text/plain")
-// 			w.WriteHeader(http.StatusOK)
-// 			_, _ = w.Write([]byte("pong"))
-// 			return
-// 		}
-// 		next.ServeHTTP(w, r)
-// 	}
-// 	return http.HandlerFunc(fn)
-// }
-
 func (s Server) getHandler(w http.ResponseWriter, r *http.Request) {
     log.Printf("[INFO] getHandler")
 
